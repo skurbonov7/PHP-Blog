d4b5eb87ddd86e607c1d078c27c7618da17b9c2b
Sorbon Kurbonov <sorbonkurbonov7@gmail.com>
2023-04-03 23:33:10 +0300

    Added templating package twig

--- index.php
+++ index.php
@@ -3,8 +3,13 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 require __DIR__ . '/vendor/autoload.php';
+
+$loader = new FilesystemLoader('templates');
+$view = new Environment($loader);
 
 // Instantiate App
 $app = AppFactory::create();
@@ -13,8 +18,16 @@ $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 // Add routes
-$app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write('Hello');
+$app->get('/', function (Request $request, Response $response) use ($view) {
+    $body = $view->render('index.twig');
+    $response->getBody()->write($body);
+    return $response;
+});
+$app->get('/about', function (Request $request, Response $response) use ($view) {
+    $body = $view->render('about.twig', [
+        'name' => 'Max'
+    ]);
+    $response->getBody()->write($body);
     return $response;
 });
 
